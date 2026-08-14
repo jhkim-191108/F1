cbe70abc247d0d87fbafcb7a32d98f4acafd16ab
JHJ1995 <bits00@naver.com>
2026-08-14 17:13:44 +0900

    feat: 페이지 js파일 api url연결

--- search.js
+++ search.js
@@ -4,3 +4,19 @@
     3. */
 
 const API_URL = "https://carrot.techfree.kr/api";
+
+async function getProducts() {
+    try{
+        const response = await fetch(`${API_URL}/api/products`);
+
+        if(!response.ok) throw new Error(response.status);
+    
+    const data = await response.json();
+    console.log(data);
+
+    } catch (error) {
+        console.error("실패", error.message);
+    }
+}
+
+getProducts();
