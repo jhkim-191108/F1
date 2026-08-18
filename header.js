d412f90b532cba6c4e7c982b080221e54bbb9306
JHJ1995 <bits00@naver.com>
2026-08-18 14:15:53 +0900

    feat: 헤더 css반응형 추가

--- header.js
+++ header.js
@@ -9,7 +9,7 @@ function headerStatus(isLoggedIn) {
     loginStatus.hidden = !isLoggedIn;
 }
 
-headerStatus(true);
+headerStatus();
 // 로그인 상태가 true라면 logoutStatus숨김 false면 반대
 // lohinStatus에서 로그아웃을 누르면 다시 logoutStatus가 나타나고 loginStatus가 hidden으로 돌아가는 이벤트 리스너
 logoutButton.addEventListener("click", function () { headerStatus(false); });
