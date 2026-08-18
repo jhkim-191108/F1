9697ea2cae7a74abf5268a715b03cc455de71876
JHJ1995 <bits00@naver.com>
2026-08-18 23:02:51 +0900

    refactor: 헤더 글자 페이지별 색상 및 비로그인 상태 동네인증 숨김

--- header.js
+++ header.js
@@ -2,14 +2,53 @@
 const loginStatus = document.querySelector("#loginStatusHeader");
 const logoutStatus = document.querySelector("#logoutStatusHeader");
 const logoutButton = document.querySelector("#upperLogoutButton");
+const tradeNav = document.querySelector("#tradeNav")
+const locationNav = document.querySelector("#locationNav")
+const profile = document.querySelector("#profileButton")
+const profileDropdown = document.querySelector("#dropdownMenu")
+
 
 // 로그인 상태를 전달받는 함수
 function headerStatus(isLoggedIn) {
     logoutStatus.hidden = isLoggedIn;
     loginStatus.hidden = !isLoggedIn;
+
+    locationNav.hidden = !isLoggedIn;
 }
 
 headerStatus();
 // 로그인 상태가 true라면 logoutStatus숨김 false면 반대
 // lohinStatus에서 로그아웃을 누르면 다시 logoutStatus가 나타나고 loginStatus가 hidden으로 돌아가는 이벤트 리스너
 logoutButton.addEventListener("click", function () { headerStatus(false); });
+
+function updateActiveNav() {
+    const currentPage = location.pathname;
+    const tradePages = [
+        "trade.html",
+        "trade_post.html",
+        "write.html",
+        "search.html",
+        "chat.html"
+    ];
+    const isTradePage = tradePages.some(function (page) {
+        return currentPage.endsWith(page);
+    });
+
+    tradeNav.classList.remove("activeNav");
+    locationNav.classList.remove("activeNav");
+
+    // tradepages에 activeNav추가
+    if(isTradePage) {
+        tradeNav.classList.add("activeNav");
+    }
+    // location에 active추가
+    if(currentPage.endsWith("/location.html")) {
+        locationNav.classList.add("activeNav");
+    }
+}
+
+updateActiveNav();
+
+profile.addEventListener("click", function(){
+    profileDropdown.hidden = !profileDropdown.hidden;
+});
